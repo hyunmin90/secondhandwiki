06619fec550864bac2f5507d5f4c82964a3c7d0a
Cheng Han Lee <Leech@Leech.local>
2014-03-03 16:56:16 -0600

    test

--- manhattan/settings.py
+++ manhattan/settings.py
@@ -81,6 +81,16 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Template files
+import os.path
+PROJECT_DIR = os.path.dirname(__file__) # this is not Django setting.
+TEMPLATE_DIRS = (
+    os.path.join(PROJECT_DIR, "templates"),
+)
+
+
+
+
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
--- manhattan/urls.py
+++ manhattan/urls.py
@@ -9,4 +9,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^test/', direct_to_template, {'template':'test.html'}),
 )
