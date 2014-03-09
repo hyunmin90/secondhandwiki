c7dbf8aceb459a349a4d19705dc86f59ccba1693
Cheng Han Lee <Leech@Leech.local>
2014-03-08 18:18:33 -0600

    asd

--- manhattan/urls.py
+++ manhattan/urls.py
@@ -11,7 +11,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'manhattan.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', '
     url(r'^admin/', include(admin.site.urls)),
     url(r'^test/$', TemplateView.as_view(template_name='test.html'),name="test"),
 )
