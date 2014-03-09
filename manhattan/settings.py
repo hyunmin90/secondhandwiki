11d0e2a6e0541a4f56ee7b66aae5bd0981fcc900
Cheng Han Lee <Leech@Leech.local>
2014-03-08 19:04:34 -0600

    s

--- manhattan/settings.py
+++ manhattan/settings.py
@@ -58,9 +58,10 @@ WSGI_APPLICATION = 'manhattan.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
 
-        'ENGINE': 'django.db.backends.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'manhattan_db',
     }
 }
 
