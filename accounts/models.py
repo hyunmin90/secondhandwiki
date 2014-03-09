e236565c609e990693879a964a121ec435a3a7b4
Cheng Han Lee <Leech@wirelessprvnat-172-16-172-62.near.illinois.edu>
2014-03-09 16:45:32 -0500

    s

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OnetoOneField(User)
+    join_date = models.DateTimeField(auto_now=True)
+    rating = models.IntegerField(default=0, blank=True, null=True)
+    GENDER_CHOICES = (('M', 'Male'),('F', 'Female'),)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+
