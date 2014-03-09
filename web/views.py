10b75ed9cd053b5f2226e6121bbbc1417816858c
Cheng Han Lee <Leech@wirelessprvnat-172-16-172-62.near.illinois.edu>
2014-03-09 17:19:38 -0500

    s

--- web/views.py
+++ web/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, render_to_response
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect, HttpResponse
+
 
 def landing(request):
     if request.user.is_authenticated():
         return render(request, 'main.html')
     else:
-        return render(request, 'sign_up.html')
+        return HttpResponseRedirect('/login')
