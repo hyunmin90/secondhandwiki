0411b17b9602374aa47f3b57ee39413ec8992539
Cheng Han Lee <Leech@Leech.local>
2014-03-09 22:01:53 -0500

    s

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth import logout
 
