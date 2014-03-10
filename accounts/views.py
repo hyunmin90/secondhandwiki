5e9e6c41d6604bcc7138208ac8abb21d9410782d
Cheng Han Lee <Leech@Leech.local>
2014-03-09 23:10:03 -0500

    s

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_protect
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, HttpResponse
-from django.contrib.auth import authenticate, login as auth_login, logout
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 
 from accounts.models import *
 
@@ -13,7 +13,7 @@ def login(request):
 
 @login_required
 def logout(request):
-    logout(request)
+    auth_logout(request)
     return HttpResponseRedirect('/')
 
 @csrf_protect
