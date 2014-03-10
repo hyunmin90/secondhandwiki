5413504fbb35581fffa57dab0940021207ab2027
Cheng Han Lee <Leech@Leech.local>
2014-03-09 21:01:48 -0500

    s

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 def login(request):
     return render(request, 'login.html', {})
 
-def login(request):
+def sign_up(request):
     if request.method == 'POST':
         pass
     else:
