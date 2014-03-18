bb4456474b89e23975a4c6789ae53aabe75eea8e
Cheng Han Lee <Leech@Leech.local>
2014-03-18 11:32:13 -0500

    s

--- products/models.py
+++ products/models.py
@@ -5,22 +5,22 @@ class Products(models.Model):
     product_name = models.CharField(max_length=100,blank=False)
     description = models.CharField(max_length=1000,blank=True,null=True)
     image = models.CharField(max_length=100,blank=True,null=True)
-    category = models.ForeignKey('Categories')
+    category = models.ForeignKey(Categories)
     slug = models.CharField(max_length=100, unique=True)
   
 class Features(models.Model):
     feature_name = models.CharField(max_length=100)
     description = models.CharField(max_length=1000,blank=True,null=True)
-    product = models.ForeignKey('Products')
+    product = models.ForeignKey(Products)
 
 class Comments(models.Model):
-    author = models.ForeignKey('accounts.Profile')
-    product = models.ForeignKey('Products')
+    author = models.ForeignKey(accounts.Profile)
+    product = models.ForeignKey(Products)
     body = models.CharField(max_length=1000)
 
 class Categories(models.Model):
     category_name = models.CharField(max_length=100)
-    parent_category = models.ForeignKey('Categories', blank=True, null=True)
+    parent_category = models.ForeignKey(Categories, blank=True, null=True)
     
 
 
