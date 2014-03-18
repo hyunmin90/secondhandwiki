d9669b03cf734784b6cab4896bd806901a02feaf
Cheng Han Lee <Leech@Leech.local>
2014-03-17 23:32:52 -0500

    s

--- products/views.py
+++ products/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 
-from Products.models import Products, Features, Comments, Categories 
+from products.models import Products, Features, Comments, Categories 
 
 
 import re
