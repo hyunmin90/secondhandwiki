8477b211dceb8f0812fa81308a034c539a62b7d9
Cheng Han Lee <Leech@Leech.local>
2014-03-18 00:37:13 -0500

    s

--- products/views.py
+++ products/views.py
@@ -31,23 +31,22 @@ def new_product(request):
         cursor = connection.cursor()
 
         #category_list = Categories.objects.raw('SELECT * FROM products_categories WHERE category_name=%s', [category])
-        cursor.execute('SELECT * FROM products_categories WHERE category_name=%s', [category])
+        cursor.execute("SELECT * FROM products_categories WHERE category_name=%s", [category])
         category_list = cursor.fetchone()
-        category_list = list(category_list)
         if len(category_list)>0: # Category exists
             the_category = category_list[0]
             #Products.objects.raw('INSERT INTO products_products(product_name, description,category,slug) VALUES(%s, %s, %s, %s)' , [product_name, description, the_category.id, slug])
-            cursor.execute('INSERT INTO products_products(product_name, description,category,slug) VALUES(%s, %s, %s, %s)' , [product_name, description, the_category.id, slug])
+            cursor.execute("INSERT INTO products_products(product_name, description,category,slug) VALUES(%s, %s, %s, %s)" , [product_name, description, the_category.id, slug])
 
         else:   # does not exist
             # add new category
             #Categories.objects.raw('INSERT INTO products_categories(category_name) VALUES(%s)', [category])
-            cursor.execute('INSERT INTO products_categories(category_name) VALUES(%s)', [category])
+            cursor.execute("INSERT INTO products_categories(category_name) VALUES(%s)", [category])
 
             category_id = len(category_list)+1
         
             #Products.objects.raw('INSERT INTO products_products(product_name, description, category, slug) VALUES(%s, %s, %s, %s)' , [product_name, description, category_id, slug])
-            cursor.execute('INSERT INTO products_products(product_name, description, category, slug) VALUES(%s, %s, %s, %s)' , [product_name, description, category_id, slug])
+            cursor.execute("INSERT INTO products_products(product_name, description, category, slug) VALUES(%s, %s, %s, %s)" , [product_name, description, category_id, slug])
 
         return HttpResponseRedirect('/products/view_product/'+slug)
     else:
