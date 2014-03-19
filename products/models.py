f86fbe73bbabcfd36ab9d44ec19e034a7b0d9c9b
Cheng Han Lee <Leech@Leech.local>
2014-03-19 18:44:41 -0500

    s

--- products/models.py
+++ products/models.py
@@ -14,17 +14,22 @@ class Products(models.Model):
     image = models.CharField(max_length=100,blank=True,null=True)
     category = models.ForeignKey(Categories)
     slug = models.CharField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
   
 class Features(models.Model):
     feature_name = models.CharField(max_length=100)
     description = models.CharField(max_length=1000,blank=True,null=True)
     product = models.ForeignKey(Products)
     author = models.ForeignKey(User)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 class Comments(models.Model):
     product = models.ForeignKey(Products)
     body = models.CharField(max_length=1000)
     author = models.ForeignKey(User)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
 
     
 
