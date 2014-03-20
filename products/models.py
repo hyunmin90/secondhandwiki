167515fb0aa9634b0dd496f5455f7fcf34e0aab8
Cheng Han Lee <Leech@Leech.local>
2014-03-19 20:48:54 -0500

    s

--- products/models.py
+++ products/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 from accounts.models import Profile
 
+import datetime
 
 class Categories(models.Model):
     category_name = models.CharField(max_length=100)
@@ -14,7 +15,7 @@ class Products(models.Model):
     image = models.CharField(max_length=100,blank=True,null=True)
     category = models.ForeignKey(Categories)
     slug = models.CharField(max_length=100, unique=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(default=datetime.datetime.now)
     
   
 class Features(models.Model):
@@ -22,13 +23,13 @@ class Features(models.Model):
     description = models.CharField(max_length=1000,blank=True,null=True)
     product = models.ForeignKey(Products)
     author = models.ForeignKey(User)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(default=datetime.datetime.now)
 
 class Comments(models.Model):
     product = models.ForeignKey(Products)
     body = models.CharField(max_length=1000)
     author = models.ForeignKey(User)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(default=datetime.datetime.now)
     
 
     
