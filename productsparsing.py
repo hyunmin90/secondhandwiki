74fd5817d2b53955684a8c3a8112a4837082418b
ethanyishchan <yish.chan@gmail.com>
2014-04-26 04:59:46 -0500

    hi

--- productsparsing.py
+++ productsparsing.py
@@ -4,38 +4,42 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "manhattan.settings")
 from products.models import Products, Categories, Features, ProductTags, Comments
 from time import sleep
 import amazonproduct
-
+from amazonproduct import NoExactMatchesFound
 def amazonPriceSearch( query ):
     from lxml import etree
     #API Access keys
-    config = {
-              'access_key': 'AKIAIKDCGPZIH4ZFFQGQ',
-              'secret_key': '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',
-              'associate_tag' : 'themanhproj-20',
-              'locale' : 'us'
-              }
-    #configuration of API
-    api = amazonproduct.API(cfg = config)
-    #search for related items
-    items = api.item_search('All', Keywords = query)
-    Price = 0.00
-    #looks at first search result and lookups the price for it
-    for stuff in items:
-        #gets the ASIN number for the product
-        ASIN = stuff.ASIN
-        asin_string = '%s' %(ASIN)
-        result = api.item_lookup(asin_string, ResponseGroup = 'OfferSummary')
-        for item in result.Items.Item:
-            try:
-	            # print 'Name of Product: %s' %(stuff.ItemAttributes.Title)
-            	# print 'URL Link of Product: %s' %(stuff.DetailPageURL)
-                Price = int(item.OfferSummary.LowestNewPrice.Amount)/100.0
-            except AttributeError:
-                x = 0
-            print 'Price amount: %s' %(Price)
-            print " "
-        #only want first search result, usually the most accurate
-        break;
+
+    try:
+	    config = {
+	              'access_key': 'AKIAIKDCGPZIH4ZFFQGQ',
+	              'secret_key': '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',
+	              'associate_tag' : 'themanhproj-20',
+	              'locale' : 'us'
+	              }
+	    #configuration of API
+	    api = amazonproduct.API(cfg = config)
+	    #search for related items
+	    items = api.item_search('All', Keywords = query)
+	    Price = 0.00
+	    #looks at first search result and lookups the price for it
+	    for stuff in items:
+	        #gets the ASIN number for the product
+	        ASIN = stuff.ASIN
+	        asin_string = '%s' %(ASIN)
+	        result = api.item_lookup(asin_string, ResponseGroup = 'OfferSummary')
+	        for item in result.Items.Item:
+	            try:
+		            # print 'Name of Product: %s' %(stuff.ItemAttributes.Title)
+	            	# print 'URL Link of Product: %s' %(stuff.DetailPageURL)
+	                Price = int(item.OfferSummary.LowestNewPrice.Amount)/100.0
+	            except AttributeError:
+	                x = 0
+	            print 'Price amount: %s' %(Price)
+	            print " "
+	        #only want first search result, usually the most accurate
+	        break;
+    except NoExactMatchesFound:
+    	x = 0
     return Price
 
 def amazonListSearch( query, num_results ):
@@ -49,45 +53,48 @@ def amazonListSearch( query, num_results ):
               }
     #configuration of API
     api = amazonproduct.API(cfg = config)
-    #search for related items
-    items = api.item_search('All', Keywords = query)
-    Price = 0.00
-    count = 0
-    #product name, image url, features concatenated into a string
-    product_list = []
     try:
-	    #looks at first search result and lookups the price for it
-	    for stuff in items:
-	        #gets the ASIN number for the product
-	        ASIN = stuff.ASIN
-	        print ASIN
-	        asin_string = '%s' %(ASIN)
-	        imagesresult = api.item_lookup(asin_string, ResponseGroup = 'Images')
-	        newproduct = "hi"
-	        productname = '%s' %(stuff.ItemAttributes.Title)
-	        for item in imagesresult.Items.Item:
-	            tempURL = item.LargeImage.URL
-	            imageURL = '%s' %tempURL
-	            break
-	        
-	        mediumresult = api.item_lookup(asin_string, ResponseGroup = 'Large')
-	        for item in mediumresult.Items.Item:
-	            featurecount = 0
-	            description = ''
-	            for stuff in item.ItemAttributes.Feature:
-	                converted_stuff = '%s' %(stuff)
-	                description += converted_stuff +', '
-	                featurecount+=1
-	                if featurecount == 4:
-	                    break
-	        if count == num_results:
-	            break
-	        count+=1
-	        
-	        newproduct = (productname,imageURL,description)
-	        product_list.append(newproduct)
-    except AttributeError:
-    	x = 0
+	    #search for related items
+	    items = api.item_search('All', Keywords = query)
+	    Price = 0.00
+	    count = 0
+	    #product name, image url, features concatenated into a string
+	    product_list = []
+	    try:
+		    #looks at first search result and lookups the price for it
+		    for stuff in items:
+		        #gets the ASIN number for the product
+		        ASIN = stuff.ASIN
+		        print ASIN
+		        asin_string = '%s' %(ASIN)
+		        imagesresult = api.item_lookup(asin_string, ResponseGroup = 'Images')
+		        newproduct = "hi"
+		        productname = '%s' %(stuff.ItemAttributes.Title)
+		        for item in imagesresult.Items.Item:
+		            tempURL = item.LargeImage.URL
+		            imageURL = '%s' %tempURL
+		            break
+		        
+		        mediumresult = api.item_lookup(asin_string, ResponseGroup = 'Large')
+		        for item in mediumresult.Items.Item:
+		            featurecount = 0
+		            description = ''
+		            for stuff in item.ItemAttributes.Feature:
+		                converted_stuff = '%s' %(stuff)
+		                description += converted_stuff +', '
+		                featurecount+=1
+		                if featurecount == 4:
+		                    break
+		        if count == num_results:
+		            break
+		        count+=1
+		        
+		        newproduct = (productname,imageURL,description)
+		        product_list.append(newproduct)
+	    except AttributeError:
+	    	x = 0
+	except NoExactMatchesFound:
+		x = 0
 
     return product_list
 
